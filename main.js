80437141b345597bf37fcd6fb592f4ec4cdfcf77
Vikrant <85709371+Vikrant-V28@users.noreply.github.com>
2022-01-13 14:09:32 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("mousemove", parallaxText);
+        function parallaxText(e) {
+            this.querySelectorAll(".layer").forEach(layer => {
+                let x = (window.innerWidth - e.pageX * 2) / 2;
+                layer.style.transform = `translateX(${x}px)`;
+            });
+        }
